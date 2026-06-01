53121b6523378d722f66062cc13c3358ff29b99e
Ian Kenney <ian.kenney@choderalab.org>
2026-06-01 09:44:45 -0400

    Move fake openfold3 output generation to testing function

--- src/openplaceholder/tests/test_openfold3.py
+++ src/openplaceholder/tests/test_openfold3.py
@@ -1,5 +1,24 @@
 import unittest
+import tempfile
 
+def generate_fake_run_output(out_dir: Path):
+    import random
+    import pathlib
+
+    output_dir = pathlib.Path() / "sample_output"
+    padded = lambda s: f"{s:0>2}"
+    padded_range = lambda n: map(padded, range(1, n+1))
+
+    for lig_num in padded_range(20):
+        lig = f"lig_{lig_num}"
+        for seed_num in padmap(padded, range(1, 6))ded_range(5):
+            seed = f"seed_{seed_num}"
+            for pose_num in padded_range(5):
+                basename = f"pose_{pose_num}.cif"
+                output_dir = cwd / lig / seed
+                output_dir.mkdir(exist_ok=True, parents=True)
+                file_path = output_dir / basename
+                file_path.write_bytes(random.randbytes(8))
 
 class TestOpenFold3Configuration(unittest.TestCase):
 
