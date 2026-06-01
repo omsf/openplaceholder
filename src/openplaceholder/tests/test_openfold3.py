718db7cbf968fb5a25dfc793e5cc25bf8d9ecb79
Ian Kenney <ian.kenney@choderalab.org>
2026-06-01 11:29:30 -0400

    Fix fake data generator

--- src/openplaceholder/tests/test_openfold3.py
+++ src/openplaceholder/tests/test_openfold3.py
@@ -1,8 +1,9 @@
 import tempfile
 import unittest
+from pathlib import Path
 
 
-def generate_fake_run_output(out_dir: Path):
+def generate_fake_run_output(out_dir: Path) -> None:
     import pathlib
     import random
 
@@ -10,13 +11,13 @@ def generate_fake_run_output(out_dir: Path):
     padded = lambda s: f"{s:0>2}"
     padded_range = lambda n: map(padded, range(1, n + 1))
 
-    for lig_num in padded_range(20):
+    for lig_num in padded_range(20):  # type: ignore
         lig = f"lig_{lig_num}"
-        for seed_num in padded_range(5):
+        for seed_num in padded_range(5):  # type: ignore
             seed = f"seed_{seed_num}"
-            for pose_num in padded_range(5):
+            for pose_num in padded_range(5):  # type: ignore
                 basename = f"pose_{pose_num}.cif"
-                output_dir = cwd / lig / seed
+                output_dir = out_dir / lig / seed
                 output_dir.mkdir(exist_ok=True, parents=True)
                 file_path = output_dir / basename
                 file_path.write_bytes(random.randbytes(8))
