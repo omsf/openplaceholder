29b726a58135a3c84a7d2654a8b7895230efc25d
Ian Kenney <ian.kenney@choderalab.org>
2026-06-02 11:29:42 -0400

    Rough sketch

--- src/openplaceholder/impl/validators.py
+++ src/openplaceholder/impl/validators.py
@@ -16,3 +16,35 @@ class PosebustersValidator(Validator):
 
     def validate(self, structures: list[Structure]) -> list[Structure]:
         raise NotImplementedError
+
+
+@dataclass(frozen=True, eq=True)
+class StereoValidatorConfig:
+    pass
+
+
+class StereoValidator(Validator):
+
+    def __init__(self, config: StereoValidatorConfig):
+        self._config = config
+
+    def validate(self, structures: list[Structure]) -> list[Structure]:
+
+        passed = []
+        failed = []
+        for structure in structures:
+
+            original_smiles = structure.smiles
+            derived_smiles = self._determine_smiles_from_bytes(structure.structure, structure.structure_format)
+
+            if self._compatible(original_smiles, derived_smiles):
+                passed.append(structure)
+            else:
+                failed.append(structure)
+
+        # NOTE: I plan to extend this to also include failures in the output
+        return passed
+
+    def _determine_smiles_from_bytes(self, structure_bytes, structure_format):
+        # use whatever reader to take the decoded file contents according to the format specified
+        raise NotImplementedError
