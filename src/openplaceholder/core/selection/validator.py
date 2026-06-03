e52ce56b11f925da7cf9cb5b57d5ad6e173d2de3
Ian Kenney <ian.kenney@choderalab.org>
2026-06-03 10:12:46 -0400

    Refactor Validator to require an implemented `_validate_structure` method

--- src/openplaceholder/core/selection/validator.py
+++ src/openplaceholder/core/selection/validator.py
@@ -5,6 +5,9 @@ from openplaceholder.core.structure.structure import Structure
 
 class Validator(ABC):
 
-    @abstractmethod
     def validate(self, structures: list[Structure]) -> list[Structure]:
+        return list(filter(self._validate_structure, structures))
+
+    @abstractmethod
+    def _validate_structure(self, structure: Structure) -> bool:
         raise NotImplementedError
