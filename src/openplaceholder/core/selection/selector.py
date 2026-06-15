aa4a4ee80dd678fd440b3c09e7480a5119dd7c6d
Ian Kenney <ian.kenney@choderalab.org>
2026-06-15 10:44:29 -0400

    Rework Selector
    Selectors previously would take a list of structures representing the
    same ligand-protein pair and return the structure that be used in
    further calculations. This decision really cannot be made in isolation
    with respect to the other ligand-protein pairs that will be included
    in the downstream AlchemicalNetwork. Therefore, we are now requiring
    that Selectors take lists of StructureSets.

--- src/openplaceholder/core/selection/selector.py
+++ src/openplaceholder/core/selection/selector.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
 
-from openplaceholder.core.structure.structure import Structure
+from openplaceholder.core.structure.structure import Structure, StructureSet
 
 
 class Selector(ABC):
 
     @abstractmethod
-    def select(self, structures: list[Structure]) -> Structure:
+    def select(self, structures: list[StructureSet]) -> list[Structure]:
         raise NotImplementedError
--- src/openplaceholder/impl/selectors.py
+++ src/openplaceholder/impl/selectors.py
@@ -1,17 +1,7 @@
 from dataclasses import dataclass
 
 from openplaceholder.core.selection.selector import Selector
-from openplaceholder.core.structure.structure import Structure
-
-
-@dataclass(frozen=True, eq=True)
-class FirstSelectorConfig:
-    pass
-
-
-@dataclass(frozen=True, eq=True)
-class RandomSelectorConfig:
-    pass
+from openplaceholder.core.structure.structure import Structure, StructureSet
 
 
 @dataclass(frozen=True, eq=True)
@@ -19,30 +9,10 @@ class CoordinationSelectorConfig:
     pass
 
 
-class FirstSelector(Selector):
-
-    def __init__(self, config: FirstSelectorConfig):
-        self._config = config
-
-    def select(self, structures: list[Structure]) -> Structure:
-        return structures[0]
-
-
-class RandomSelector(Selector):
-
-    def __init__(self, config: RandomSelectorConfig):
-        self._config = config
-
-    def select(self, structures: list[Structure]) -> Structure:
-        import random
-
-        return random.choice(structures)
-
-
 class CoordinationSelector(Selector):
 
     def __init__(self, config: CoordinationSelectorConfig):
         self._config = config
 
-    def select(self, structures: list[Structure]) -> Structure:
+    def select(self, structures: list[StructureSet]) -> Structure:
         raise NotImplementedError
