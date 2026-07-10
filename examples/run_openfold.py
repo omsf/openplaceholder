0ac88a3be428750a5cd1098ad1c17cf38db729ab
Ian Kenney <ian.kenney@choderalab.org>
2026-07-10 15:39:57 -0400

    Update example script to use config.toml

--- examples/run_openfold.py
+++ examples/run_openfold.py
@@ -1,45 +1,59 @@
+#!/usr/bin/env python3
+
 import os
 import random
 from pathlib import Path
+import tomllib
 
 from openplaceholder.impl.generator.openfold3 import (
     OpenFold3Generator,
     OpenFold3GeneratorConfig,
 )
 
+TOML_CONFIG = Path(__file__).parents[1] / "config.toml"
+
 if __name__ == "__main__":
 
-    num_seeds = 5
-    gen_dir = Path() / "output_directory"
-    clean_up = False
+    if not TOML_CONFIG.exists():
+        raise FileNotFoundError(f"Could not find {TOML_CONFIG}")
 
-    if not (run_openfold_path := os.getenv("RUN_OPENFOLD_PATH")):
-        raise ValueError("Missing RUN_OPENFOLD_PATH")
+    config = tomllib.loads(TOML_CONFIG.read_text())
+    generator_config = config["generation"]["generator"]
 
-    # tyk2
-    sequence = "TVFHKRYLKKIRDLGEGHFGKVSLYCYDPTNDGTGEMVAVKALKADCGPQHRSGWKQEIDILRTLYHEHIIKYKGCCEDQGEKSLQLVMEYVPLGSLRDYLPRHSIGLAQLLLFAQQICEGMAYLHAQHYIHRDLAARNVLLDNDRLVKIGDFGLAKAVPEGHEYYRVREDGDSPVFWYAPECLKEYKFYYASDVWSFGVTLYELLTHCDSSQSPPTKFLELIGIAQGQMTVLRLTELLERGERLPRPDKCPCEVYHLMKNCWETEASFRPTFENLIPILKTVHEKYQ"
-    ligands = {
-        "lig_ejm_31": "CC(=O)Nc1cc(NC(=O)c2c(Cl)cccc2Cl)ccn1",
-        "lig_ejm_42": "CCC(=O)Nc1cc(NC(=O)c2c(Cl)cccc2Cl)ccn1",
-        "lig_ejm_43": "CC(C)C(=O)Nc1cc(NC(=O)c2c(Cl)cccc2Cl)ccn1",
-        "lig_ejm_46": "O=C(Nc1ccnc(NC(=O)C2CC2)c1)c1c(Cl)cccc1Cl",
-        "lig_ejm_47": "O=C(Nc1ccnc(NC(=O)C2CCC2)c1)c1c(Cl)cccc1Cl",
-        "lig_ejm_48": "O=C(Nc1ccnc(NC(=O)C2CCCC2)c1)c1c(Cl)cccc1Cl",
-        "lig_ejm_50": "O=C(CO)Nc1cc(NC(=O)c2c(Cl)cccc2Cl)ccn1",
-        "lig_jmc_23": "O=C(Nc1ccnc(NC(=O)[C@H]2C[C@H]2F)c1)c1c(Cl)cccc1Cl",
-        "lig_jmc_27": "O=C(Nc1ccnc(NC(=O)[C@H]2C[C@H]2Cl)c1)c1c(Cl)cccc1Cl",
-        "lig_jmc_28": "C[C@@H]1C[C@@H]1C(=O)Nc1cc(NC(=O)c2c(Cl)cccc2Cl)ccn1",
-    }
+    # normally this would be set in the config directly, but that is
+    # impractical for this example
+    generator_config["run_openfold_path"] = os.getenv("RUN_OPENFOLD_PATH")
+
+    if gendir := os.getenv("GEN_DIR_OVERRIDE"):
+        generator_config["generator_directory"] = gendir
+
+    if not generator_config["implementation"] == "openplaceholder.impl.generator.openfold3:OpenFold3Generator":
+        raise ValueError("This script is only configured to run the OpenFold3Generator")
+
+
+    # in theory you could double splat these into the
+    # OpenFold3Generator, but it's explicitly unpacked here for
+    # demonstration.
+    num_seeds = generator_config["generate_n_seeds"]
+    n_diffusion_samples = generator_config["n_diffusion_samples"]
+    gen_dir = Path(generator_config["generator_directory"])
+    clean_up = generator_config.get("generate_n_seeds") or False
+    sequence = generator_config["sequence"]
+    ligands = generator_config["ligands"]
+    run_openfold_path = os.getenv("RUN_OPENFOLD_PATH")
 
     config = OpenFold3GeneratorConfig(
         sequence=sequence,
         ligands=ligands,
-        n_diffusion_samples=5,
+        n_diffusion_samples=n_diffusion_samples,
         generator_directory=gen_dir,
         run_openfold_path=run_openfold_path,
         generate_n_seeds=num_seeds,
         clean_up=clean_up,
     )
+
+    if run_openfold_path is None:
+        raise ValueError("Missing RUN_OPENFOLD_PATH")
 
     generator = OpenFold3Generator(config)
     generator.run()
