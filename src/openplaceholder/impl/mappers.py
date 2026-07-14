4a118c8d99cf95cb2c1d76b51b7001feddac9541
Ian Kenney <ian.kenney@choderalab.org>
2026-07-14 08:47:11 -0400

    Add _create_ligand_network

--- src/openplaceholder/impl/mappers.py
+++ src/openplaceholder/impl/mappers.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
+import logging
 
-from gufe import AlchemicalNetwork
+from gufe import AlchemicalNetwork, LigandNetwork, Protocol
 
 from openplaceholder.core.assembly.mapper import Mapper, MapperConfigBase
-from openplaceholder.core.structure import Structure
+from openplaceholder.core.structure import Structure, LigandPerceptionError
 
+logger = logging.getLogger(__name__)
 
 @dataclass(frozen=True)
 class LOMAPMapperConfig(MapperConfigBase): ...
@@ -22,7 +24,7 @@ class LOMAPMapper(Mapper):
 
 @dataclass(frozen=True)
 class KartografMapperConfig(MapperConfigBase):
-    central_ligand: str | None = None
+    pass
 
 class KartografMapper(Mapper):
 
@@ -32,4 +34,33 @@ class KartografMapper(Mapper):
         pass
 
     def _map(self, structures: list[Structure]) -> AlchemicalNetwork:
+        ligand_network = self._create_ligand_network(structures)
         raise NotImplementedError
+
+    @staticmethod
+    def _create_protocol() -> Protocol:
+        from openfe import RelativeHybridTopologyProtocol
+        return RelativeHybridTopologyProtocol(RelativeHybridTopologyProtocol.default_settings())
+
+    @staticmethod
+    def _create_ligand_network(structures: list[Structure]) -> LigandNetwork:
+        ligands = []
+        logger.debug("building ligand network from %d structures", len(structures))
+        for structure in structures:
+            try:
+                logger.debug("recovering ligand %s from structure", structure.ligand_name)
+                mol = structure.to_rdkit_ligand_mol(selection="resname UNL")
+            except LigandPerceptionError as e:
+                raise e
+            logger.debug("added %s to ligand network ligands", structure.ligand_name)
+            ligands.append(mol)
+
+        mappers = [openfe.setup.KartografAtomMapper()]
+        scorer = openfe.lomap_scorers.default_lomap_score
+
+        network = openfe.ligand_network_planning.generate_minimal_spanning_network(
+            ligands = ligands,
+            mappers = mappers,
+            scorer = scorer,
+        )
+        return network
