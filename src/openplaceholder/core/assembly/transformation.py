9df007626d8371488e21c6670b08b03afb0b8cdb
Ian Kenney <ian.kenney@choderalab.org>
2026-07-22 16:19:20 -0400

    Don't even attempt to transform empty lists

--- src/openplaceholder/core/assembly/transformation.py
+++ src/openplaceholder/core/assembly/transformation.py
@@ -18,5 +18,8 @@ class Transformation(Module, ABC):
         raise NotImplementedError
 
     def transform(self, structures: list[Structure]) -> list[Structure]:
+        if len(structures) == 0:
+            logger.error("%s received and empty list of structures", self.__class__.__name__)
+            raise ValueError("no structures provided to transformation")
         logger.info("transforming structures using %s", self.__class__.__name__)
         return self._transform(structures)
